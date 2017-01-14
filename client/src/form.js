4777bc48f7c21872a07a02cdd709f0967bee71b1
Bernard Hamlin <bernard.hamlin@gmail.com>
2017-01-14 13:37:23 +1300

    uploader

--- client/src/form.js
+++ client/src/form.js
@@ -1,33 +1,59 @@
-import React from 'react';
-import {Field, reduxForm} from 'redux-form';
+import React, {Component} from 'react';
+import {Field, reduxForm, SubmissionError} from 'redux-form';
 import {graphql} from 'react-apollo';
 import {compose} from 'redux';
 import FileField from './lib/fields/file';
 import gql from 'graphql-tag';
 
-function Form({handleSubmit}) {
-  return (
-    <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label htmlFor="fileName">File Name</label>
-        <Field className="form-control" name="fileName" component="input" type="text"/>
-      </div>
-      <div className="form-group">
-        <label htmlFor="upload">Upload</label>
-        <Field className="form-control" name="upload" component={FileField}/>
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  );
+class Form extends Component {
+  constructor() {
+    super();
+    this._submitter = this._submitter.bind(this);
+  }
+  _submitter(data) {
+    const {submitForm, reset} = this.props;
+    const submitter = (data) => {
+      return submitForm(data)
+      .catch((err) => {
+        if (err instanceof SubmissionError) {
+          throw err;
+        }
+
+        console.log(err);//eslint-disable-line no-console
+        throw new SubmissionError({_error: 'Error submiting form'});
+      })
+      .then(() => {
+        reset();
+        this.setState({
+          saved: true
+        });
+      });
+    };
+    return submitter(data);
+  };
+  render () {
+    const {handleSubmit} = this.props;
+    return (
+      <form onSubmit={handleSubmit(this._submitter)}>
+        <div className="form-group">
+          <label htmlFor="fileName">File Name</label>
+          <Field className="form-control" name="fileName" component="input" type="text"/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="File">Upload</label>
+          <Field className="form-control" name="File" component={FileField}/>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
 }
 
 const uploadMutation = gql`
-mutation uploadMutation {
-  updateExhibit(data:$data) {
-    data {
-      ...exhibitInfo
-    },
-    errors {key, message}
+mutation testMutation($File: Upload!, $FileName: String) {
+  testMutation(File: $File, FileName: $FileName) {
+    FileName,
+    File
   }
 }
 `;
@@ -35,20 +61,20 @@ mutation uploadMutation {
 const enhanced = compose(
   graphql(uploadMutation, {
     props: ({mutate}) => ({
-      submit: (data) => {
+      submitForm: (data) => {
         return mutate({
-          variables: formatData(data),
-          fragments: [exhibitInfoFragment, sharepointFileInfoFragment, locationInfoFragment],
-          updateQueries: {
-            exhibitQuery: (prev, {mutationResult, queryVariables}) => {
-              const updateExhibit = get(mutationResult, 'data.updateExhibit.data');
-              const {id} = updateExhibit;
-              if (updateExhibit && id) {
-                return  {...prev, exhibit: updateExhibit};
-              }
-              return prev;
-            }
-          }
+          variables: data,
+          // fragments: [exhibitInfoFragment, sharepointFileInfoFragment, locationInfoFragment],
+          // updateQueries: {
+          //   exhibitQuery: (prev, {mutationResult, queryVariables}) => {
+          //     const updateExhibit = get(mutationResult, 'data.updateExhibit.data');
+          //     const {id} = updateExhibit;
+          //     if (updateExhibit && id) {
+          //       return  {...prev, exhibit: updateExhibit};
+          //     }
+          //     return prev;
+          //   }
+          // }
         })
         .then((resp) => {
           const {data, errors} = resp.data.updateExhibit;
